a8cbfaa18aba9101a7a7a58224b0efccebc026ab
mustafaameen91 <mustafa.mohammed.ameen91@gmail.com>
2021-12-14 18:18:48 +0300

    add new routes

--- app/routes/province.routes.js
+++ app/routes/province.routes.js
@@ -1,15 +1,15 @@
 module.exports = (app) => {
    const province = require("../controllers/province.controllers.js");
 
-   app.post("/api/addProvince", province.create);
+   app.post("province/new", province.create);
 
-   app.get("/api/provinces", province.findAll);
+   app.get("/provinces", province.findAll);
 
    app.get("/api/province/:id", province.findOne);
 
-   app.put("/api/province/:id", province.update);
+   app.put("/province/edit/:id", province.update);
 
-   app.delete("/api/province/:id", province.delete);
+   app.delete("/province/delete/:id", province.delete);
 
    app.delete("/api/provinces", province.deleteAll);
 };
