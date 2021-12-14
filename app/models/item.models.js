f34755ee4a85c7cf4f414a9492babfa6a32a4414
mustafaameen91 <mustafa.mohammed.ameen91@gmail.com>
2021-12-14 19:49:48 +0300

    add new routes

--- app/models/item.models.js
+++ app/models/item.models.js
@@ -76,6 +76,7 @@ Item.getAllWithQuery = async (result) => {
          await prismaInstance.$queryRaw`SELECT *, (SELECT @totalPlus := IFNULL(SUM(count), 0) FROM invoiceContent JOIN invoice ON invoiceContent.invoiceId = invoice.idInvoice JOIN invoiceType ON invoice.invoiceTypeId = invoiceType.idInvoiceType WHERE invoiceContent.itemId = item.idItem AND invoiceType.invoiceFunction = 'plus') AS totalPlus, (SELECT @totalMinus := IFNULL(SUM(count), 0) FROM invoiceContent JOIN invoice ON invoiceContent.invoiceId = invoice.idInvoice JOIN invoiceType ON invoice.invoiceTypeId = invoiceType.idInvoiceType WHERE invoiceContent.itemId = item.idItem AND invoiceType.invoiceFunction = 'minus') AS totalMinus, (@totalPlus - @totalMinus) AS store, (SELECT GROUP_CONCAT(json_object('price',price,'sellPriceId',sellPriceId,'sellPriceName',sellPriceName)) FROM itemPrice JOIN sellPrice ON itemPrice.sellPriceId = sellPrice.idSellPrice WHERE itemPrice.itemId = item.idItem) As prices FROM item LEFT JOIN itemGroup ON item.itemGroupId = itemGroup.idItemGroup`;
       items.prices = `[${items.prices}]`;
       items.prices = JSON.stringify(items.prices);
+      console.log(items);
       result(null, items);
    } catch (err) {
       console.log(prismaErrorHandling(err));
